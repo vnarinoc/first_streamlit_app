7f771b1798e829e19dca0ff53fd47aa255aebfa6
vnarinoc <144807597+vnarinoc@users.noreply.github.com>
2023-09-15 06:50:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,6 @@ streamlit.dataframe(fruits_to_show)
 # 
 #llamar una API
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
+streamlit.text(fruityvice_response.json())
+streamlit.header("Fruityvice Fruit Advice!")
 
